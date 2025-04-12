7f05e3ccdb1b58ccc9556e98a7281ccafbe999ea
bryancsinformatica <bryancsinformatica@gmail.com>
2025-04-12 10:07:29 -0600

    add: sprint-boot initializr

---
+++ src/main/java/com/untalbry/bloggin_platform_api/BlogginPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.untalbry.bloggin_platform_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogginPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogginPlatformApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/untalbry/bloggin_platform_api/BlogginPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.untalbry.bloggin_platform_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogginPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
