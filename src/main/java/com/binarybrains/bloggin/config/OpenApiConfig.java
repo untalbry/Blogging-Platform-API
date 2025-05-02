5167911bc90df7ad1bcebb0d83bd0c769a7e93fc
bryancsinformatica <bryancsinformatica@gmail.com>
2025-05-01 23:20:04 -0600

    doc: Swagger configuration added

---
+++ src/main/java/com/binarybrains/bloggin/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.binarybrains.bloggin.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Blogging Platform API")
+                        .version("v0.11.1")
+                        .description("RESTful API for a personal blogging platform"));
+    }
+}
